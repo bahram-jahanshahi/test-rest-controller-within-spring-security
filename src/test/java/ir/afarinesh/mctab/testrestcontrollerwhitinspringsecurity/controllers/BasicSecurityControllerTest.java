d26a466602afcf535654150b3de006c784c2f9bb
Bahram Jahanshahi <jahanshahi@amnpardaz.ir>
2020-02-23 16:54:26 +0330

    Initial

--- src/test/java/ir/afarinesh/mctab/testrestcontrollerwhitinspringsecurity/controllers/BasicSecurityControllerTest.java
+++ src/test/java/ir/afarinesh/mctab/testrestcontrollerwhitinspringsecurity/controllers/BasicSecurityControllerTest.java
@@ -22,6 +22,7 @@ class BasicSecurityControllerTest {
     void basicLogin() {
         ResponseEntity<Object> result = testRestTemplate.withBasicAuth("user", "pass")
                 .getForEntity("/security/basic/login", Object.class);
+        System.out.println(result.getBody().toString());
         assertEquals(HttpStatus.OK, result.getStatusCode());
     }
 }
