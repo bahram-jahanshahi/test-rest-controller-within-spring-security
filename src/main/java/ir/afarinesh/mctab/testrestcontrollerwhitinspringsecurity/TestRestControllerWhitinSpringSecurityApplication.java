28f7dca2262b01a1fb9947870711bf154af1a027
Bahram Jahanshahi <jahanshahi@amnpardaz.ir>
2020-02-23 16:40:50 +0330

    Initial

---
+++ src/main/java/ir/afarinesh/mctab/testrestcontrollerwhitinspringsecurity/TestRestControllerWhitinSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package ir.afarinesh.mctab.testrestcontrollerwhitinspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestControllerWhitinSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRestControllerWhitinSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/afarinesh/mctab/testrestcontrollerwhitinspringsecurity/controllers/BasicSecurityControllerTest.java
@@ -0,0 +1,27 @@
+package ir.afarinesh.mctab.testrestcontrollerwhitinspringsecurity.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @WebMvcTest(BasicSecurityController.class)
+class BasicSecurityControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DisplayName("Test Basic Login")
+    void basicLogin() {
+        ResponseEntity<Object> result = testRestTemplate.withBasicAuth("user", "pass")
+                .getForEntity("/security/basic/login", Object.class);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+}
